201516249dfabce3c86896039e200bc2cfd3d103
NajwaN96 <102930213+NajwaN96@users.noreply.github.com>
2022-08-11 03:27:19 +0300

    Update kandi.js

--- kandi.js
+++ kandi.js
@@ -715,8 +715,8 @@ function animate() {
 
     // draw the score
     ctx.fillText('Score: ' + score + 'm', canvas.width - 160, 30);
-    ctx.fillText('Name: ' + 'Chandra Arya', canvas.width - 160, 45);
-    ctx.fillText('Class: ' + 'DevOps online', canvas.width - 160, 60);
+    ctx.fillText('Name: ' + 'Najwa Alshahrani', canvas.width - 160, 45);
+    ctx.fillText('Class: ' + 'DevOps onsit', canvas.width - 160, 60);
 
     // spawn a new Sprite
     if (ticker % Math.floor(platformWidth / player.speed) === 0) {
